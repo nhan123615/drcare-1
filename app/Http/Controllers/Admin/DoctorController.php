5d7365526db55d1f5ffc604acd46ed3985042bba
nhan123615 <64056052+nhan123615@users.noreply.github.com>
2020-10-12 16:25:32 +0700

    12.10.2020

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Doctor;
 use App\Models\DoctorType;
+use DB;
+
 class DoctorController extends Controller
 {
     /**
@@ -13,11 +15,69 @@ class DoctorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        if(request()->ajax())
+        {
+         if($request->occupation)
+         {
+          $data = DB::table('doctors')
+            ->join('doctor_types', 'doctor_types.id', '=', 'doctors.doctor_type_id')
+            ->select('doctors.id', 'doctors.name', 'doctor_types.name', 'doctors.photo', 'doctors.description', 'doctors.status')
+            ->where('doctors.doctor_type_id', $request->occupation);
+         }
+         else
+         {
+          $data = DB::table('doctors')
+          ->join('doctor_types', 'doctor_types.id', '=', 'doctors.doctor_type_id')
+          ->select('doctors.id', 'doctors.name', 'doctor_types.name', 'doctors.photo', 'doctors.description', 'doctors.status');
+         }
+   
+         return datatables()->of($data)
+         ->addColumn('edit', function ($data) {
+         return
+         '<td>
+         <a href="'.route('doctor.edit', $data->id).'"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> 
+         </td>';
+         })
+
+         ->addColumn('delete', function ($data) {
+            return
+            '<td class="text-center">
+            <form id="delete-form-'.$data->id.'" action="'.route('doctor.destroy',$data->id).'" style="display:none" method="post">
+              '.csrf_field().'
+              '.method_field('DELETE').'
+            </form>
+            <a href="" onclick="
+                    if(confirm('."'Are you sure, you want to delete this ?'".')){
+                      event.preventDefault();
+                      document.getElementById('."delete-form-$data->id".').submit();
+                    }else{
+                      event.preventDefault();
+                    }
+                    ">
+                    <i class="fa fa-trash  text-danger" aria-hidden="true"></i></a>
+            </td>';
+           /*  return 
+            '<a href="" onclick="alert('."'abc'".')">
+                    <i class="fa fa-trash  text-danger" aria-hidden="true"></i>
+            </a>'; */
+            })
+         -> rawColumns(['edit','delete'])
+
+    
+      
+        ->make(true);
+        }
         
-        $doctors = Doctor::all();
-        return view('admin.doctor.show',compact('doctors'));
+     
+        $occupations = DB::table('doctor_types')
+        ->select("*")
+        ->get();
+
+        
+        return view('admin.doctor.show',compact('occupations'));
+
     }
 
     /**
